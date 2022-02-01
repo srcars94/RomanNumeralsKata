ec2f82f686f704c6039797e7b263681476286375
Sam <samcarslake@hotmail.co.uk>
2022-02-01 19:01:09 +0000

    Add project files.

---
+++ RomanNumeralsKata/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RomanNumeralsKata
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
